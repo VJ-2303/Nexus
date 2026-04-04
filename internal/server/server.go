384e8b591e4e048d93bdfd397ee7799b15bdffea
VJ-2303 <vanaraj1018@gmail.com>
2026-04-04 19:25:08 +0530

    Created server package with gracefull shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,58 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func New(addr string, handler http.Handler) *Server {
+	return &Server{
+		httpServer: &http.Server{
+			Addr:         addr,
+			Handler:      handler,
+			ReadTimeout:  15 * time.Second,
+			WriteTimeout: 30 * time.Second,
+			IdleTimeout:  60 * time.Second,
+		},
+	}
+}
+
+func (s *Server) Run() error {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	errChan := make(chan error, 1)
+
+	go func() {
+		log.Printf("Nexus listening on %s", s.httpServer.Addr)
+		if err := s.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			errChan <- err
+		}
+	}()
+
+	select {
+	case sig := <-sigChan:
+		log.Printf("Received signal %v, shutting down...", sig)
+	case err := <-errChan:
+		return fmt.Errorf("server error: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		return fmt.Errorf("graceful shutdown failed: %w", err)
+	}
+	log.Println("Server stopped gracefully")
+	return nil
+}

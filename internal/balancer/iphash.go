6743fac58afc67cf9894568d229510c118d719ad
VJ-2303 <vanaraj1018@gmail.com>
2026-04-04 22:57:35 +0530

    Implemented IP Hash

---
+++ internal/balancer/iphash.go
@@ -0,0 +1,64 @@
+package balancer
+
+import (
+	"hash/fnv"
+	"net"
+	"net/http"
+	"strings"
+
+	"github.com/VJ-2303/Nexus/internal/backend"
+)
+
+type IPHash struct{}
+
+func NewIPHash() *IPHash {
+	return &IPHash{}
+}
+
+func (ih *IPHash) Next(backends []*backend.Backend, r *http.Request) *backend.Backend {
+	n := uint64(len(backends))
+
+	if n == 0 {
+		return nil
+	}
+
+	ip := extractClientIP(r)
+
+	hash := hashIP(ip)
+
+	for i := uint64(0); i < n; i++ {
+		idx := (hash + i) % n
+		b := backends[idx]
+		if b.IsAlive() {
+			return b
+		}
+	}
+	return nil
+}
+
+func extractClientIP(r *http.Request) string {
+	if xff := r.Header.Get("X-Forwarded-For"); xff != "" {
+		parts := strings.SplitN(xff, ",", 2)
+		clientIP := strings.TrimSpace(parts[0])
+
+		if host, _, err := net.SplitHostPort(clientIP); err == nil {
+			return host
+		}
+		return clientIP
+	}
+	if xri := r.Header.Get("X-Real-IP"); xri != "" {
+		return xri
+	}
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr
+	}
+	return host
+}
+
+func hashIP(ip string) uint64 {
+	h := fnv.New64a()
+	h.Write([]byte(ip))
+
+	return h.Sum64()
+}
